92775ed01c4f4a23aceeeae9d151d36663bb65a7
Jessica Costa <jesscodes@proton.me>
2023-12-04 20:50:29 -0300

    Terceiro commit

--- app.js
+++ app.js
@@ -1,62 +1,17 @@
-//Creating variables
+const userInput =  document.querySelector("#display");
+let expression = "";
 
-const calculator = document.getElementsByClassName('.calculator');
-const keys = document.getElementsByClassName('.calculator-keys');
-const display = document.getElementsByClassName('.calculator-display');
+press = (num) => {
+  expression += num;
+  userInput.value = expression;
+};
 
-//Adding event listeners
+equal = () => {
+  userInput.value = eval(expression);
+  expression = "";
+};
 
-function main () {
-    keys.addEventListener('click', e => {
-    //
-    if (e.target.matches('button')) {
-      const key = e.target;
-      const action = key.dataset.action;
-      const keyContent = key.textContent;
-      const displayedNum = display.textContent;
-      Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'));
-      calculator.dataset.previousKeyType = 'operator';
-    };
-    //
-    if (!action) {
-      if (displayedNum === '0' || previousKeyType === 'operator') {
-        display.textContent = keyContent;
-      } else {
-        display.textContent = displayedNum + keyContent;
-      };
-    };
-    //
-    if (action === 'decimal') {
-      display.textContent = displayedNum + '.';
-    };
-    //
-    if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-      key.classList.add('is-depressed');
-      calculator.dataset.firstValue = displayedNum;
-      calculator.dataset.operator = action;
-    };
-    if (action === 'calculate') {
-      const secondValue = displayedNum;
-      const firstValue = calculator.dataset.firstValue;
-      const operator = calculator.dataset.operator;
-      display.textContent = calculate(firstValue, operator, secondValue);
-    };
-    const calculate = (n1, operator, n2) => {
-    let result = '';
-    if (operator === 'add') {
-      result = parseFloat(n1) + parseFloat(n2);
-    } else if (operator === 'subtract') {
-      result = parseFloat(n1) - parseFloat(n2);
-    } else if (operator === 'multiply') {
-      result = parseFloat(n1) * parseFloat(n2);
-    } else if (operator === 'divide') {
-      result = parseFloat(n1) / parseFloat(n2);
-    };
-      return result;
-    };
-    if (action === 'clear') {
-      console.log('clear key!');
-    };
-  });
+erase = () => {
+  expression = "";
+  userInput.value = expression;
 };
